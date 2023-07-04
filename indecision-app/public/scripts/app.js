a6b1ea1b66e6985e9a31e5fe8f9871632c3ffae6
albin <albin.santhosh98@gmail.com>
2023-07-03 23:19:14 -0700

    dynamic data

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,12 +1,69 @@
-console.log("App.js is running!")
+'use strict';
 
-// JSX - JavaScript XML
-// var template = `<p> This is JSX from app.js!</p>`;
+console.log("App.js is running!");
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+};
 
 var template = React.createElement(
-    "h1", 
-    {id: "someid"},
-    "Something New"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var user = {
+    name: 'Andrew',
+    age: 26,
+    location: 'Philadelphia'
+};
+var userName = 'Albin';
+var userAge = 25;
+var userLocation = 'Gurgaon';
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 
 var appRoot = document.getElementById('app');
---
+++ indecision-app/src/app.js
@@ -0,0 +1,37 @@
+console.log("App.js is running!")
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Andrew',
+    age: 26,
+    location: 'Philadelphia'
+};
+var userName = 'Albin';
+var userAge = 25;
+var userLocation = 'Gurgaon'
+var templateTwo =(
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
