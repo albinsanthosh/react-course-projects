e654c6ca44aad23cd668211e568c151d1e18422f
albin <albin.santhosh98@gmail.com>
2023-07-09 05:11:58 -0700

    counter

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,14 +1,16 @@
 console.log("App.js is running!")
 
-var app = {
+const app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options.length > 0 ? <p>Here are your options</p> : <p>No options</p>}
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -16,22 +18,34 @@ var template = (
     </div>
 );
 
-var user = {
-    name: 'Andrew',
-    age: 26,
-    location: 'Philadelphia'
+
+let count = 0;
+const addOne = () => {
+    count++;
+    renderCounterApp();
+}
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+}
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+}
+
+
+const appRoot = document.getElementById('app');
+
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
 };
-var userName = 'Albin';
-var userAge = 25;
-var userLocation = 'Gurgaon'
-var templateTwo =(
-    <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
-    </div>
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+renderCounterApp();
