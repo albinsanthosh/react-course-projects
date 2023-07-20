18aba2a7d27d303fd4ee2a5d8a3939d377a0054a
albin <albin.santhosh98@gmail.com>
2023-07-20 07:34:39 -0700

    sourcemap for testing

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,22 +1,5 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js'
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81))
-
-// import isSenior ,{ isadult, canDrink } from "./person.js";
-
-// console.log(isadult(18));
-// console.log(canDrink(20));
-// console.log(isSenior(65));
-
-// import validator from "validator";
-// console.log(validator.isEmail('test@gmail.com'));
-
 import React from "react";
 import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
 
-const template = <p>THIS IS FROM JSX FROM WEBPACK</p>;
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={['Option one', 'Option two']}/>, document.getElementById('app'));
