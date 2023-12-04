a489cd34b95d77efa2fd3012da5c61301766e787
VF-ROOT\santa003 <albin.santhosh@vcontractor.co.za>
2023-12-04 17:44:04 +0530

    Array destructing Action generators

--- expensify-app/src/playground/destructing.js
+++ expensify-app/src/playground/destructing.js
@@ -15,14 +15,28 @@
 //     console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const { name: publisherName = 'Self-Published'} = book.publisher;
+// const { name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array destructuring 
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+// const [, city, state = 'New York'] = address;
+
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+
+const [name, , medium] = item;
+
+console.log(`A medium ${name} costs ${medium}.`);
