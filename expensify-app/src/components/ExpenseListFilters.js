4d3453ccbc5d8ae555c4a2405ef57b9c235ef28d
VF-ROOT\santa003 <albin.santhosh@vcontractor.co.za>
2023-12-20 15:47:40 +0530

    Creating Add Expense Form

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, storeByDate, storeByAmount } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
     <div>
-        <input type="text" value={props.filters.text} onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value));
-        }}/>
+        <input 
+            type="text" 
+            value={props.filters.text} 
+            onChange={(e) => {
+                props.dispatch(setTextFilter(e.target.value));
+            }}
+        />
+        <select 
+            value={props.filters.sortBy} 
+            onChange={(e) => {
+                if(e.target.value === 'date'){
+                    props.dispatch(storeByDate(e.target.value));
+                } else if (e.target.value === 'amount'){
+                    props.dispatch(storeByAmount(e.target.value));
+                }
+            }}
+        >
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+        </select>
     </div>
 );
 
