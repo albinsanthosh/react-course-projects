3b32645fe314b1b6154bfc298b583bbb2833b379
VF-ROOT\santa003 <albin.santhosh@vcontractor.co.za>
2024-03-28 17:25:08 +0530

    firebase promises

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -14,6 +14,35 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-firebase.database().ref().set({
-    name: 'Albin Santhosh'
+const database = firebase.database();
+
+database.ref().set({
+    name: 'Andrew Mead',
+    age: 26,
+    isSingle: false,
+    location: {
+        city: 'Philadelphia',
+        country: 'United States'
+    }
+}).then(() => {
+    console.log('Data is saved');
+}).catch((e) => {
+    console.log('This failed.', e);
 });
+
+// database.ref().set('This is my data');
+
+// database.ref('age').set(27);
+// database.ref('location/city').set('New York');
+
+database.ref('attributes').set({
+    height: 73,
+    weight: 150
+}).then(() => {
+    console.log('Second set call worked');
+}).catch((e) => {
+    console.log('Things didnt work for the second error', e);
+});
+
+
+// console.log('I made a request to change the data');
