de07a4339974342a74b305e0e8312196e436f232
VF-ROOT\santa003 <albin.santhosh@vcontractor.co.za>
2024-04-01 20:26:04 +0530

    testing with mock redux

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -16,33 +16,135 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Andrew Mead',
-    age: 26,
-    isSingle: false,
-    location: {
-        city: 'Philadelphia',
-        country: 'United States'
-    }
-}).then(() => {
-    console.log('Data is saved');
-}).catch((e) => {
-    console.log('This failed.', e);
-});
+export { firebase, database as default };
 
-// database.ref().set('This is my data');
+// // child_removed
+// database.ref('expenses').on('child_removed',(snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
 
-// database.ref('age').set(27);
-// database.ref('location/city').set('New York');
+// // child_changed
+// database.ref('expenses').on('child_changed',(snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
 
-database.ref('attributes').set({
-    height: 73,
-    weight: 150
-}).then(() => {
-    console.log('Second set call worked');
-}).catch((e) => {
-    console.log('Things didnt work for the second error', e);
-});
+// // child_added
+// database.ref('expenses').on('child_added',(snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
 
+// // database.ref('expenses')
+// //     .on('value', (snapshot) => {
+// //         const expenses = [];
 
-// console.log('I made a request to change the data');
+// //         snapshot.forEach((childSnapshot) => {
+// //             expenses.push({
+// //                 id: childSnapshot.key,
+// //                 ...childSnapshot.val()
+// //             });
+// //         });
+
+// //         console.log(expenses);
+// //     }, (e) => {
+// //         console.log('Error with data fetching', e);
+// //     });
+
+// // database.ref('expenses')
+// //     .once('value')
+// //     .then((snapshot) => {
+// //         const expenses = [];
+
+// //         snapshot.forEach((childSnapshot) => {
+// //             expenses.push({
+// //                 id: childSnapshot.key,
+// //                 ...childSnapshot.val()
+// //             });
+// //         });
+
+// //         console.log(expenses);
+// //     });
+
+// // database.ref('expenses').push({
+// //     description: 'Rent',
+// //     note: '',
+// //     amount: 10950,
+// //     createdAt: 1
+// // });
+
+// // database.ref('notes/-Nu908J6vIWgM5jrr7QX').remove();
+
+// // database.ref('notes').push({
+// //     title: 'Course Topics',
+// //     body: 'React Native, Angular, Python'
+// // })
+
+// // database.ref('notes').set(notes);
+
+// // const onValueChange = database.ref().on('value', (snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// // }, (e) => {
+// //     console.log('Error with data fetching', e);
+// // });
+
+// // const onValueChange = database.ref().on('value', (snapshot) => {
+// //     console.log(snapshot.val());
+// // }, (e) => {
+// //     console.log('Error with data fetching', e);
+// // });
+
+// // setTimeout(() => {
+// //     database.ref('age').set(29);
+// // }, 3500);
+
+// // setTimeout(() => {
+// //     database.ref().off(onValueChange);
+// // }, 7000);
+
+// // setTimeout(() => {
+// //     database.ref('age').set(30);
+// // }, 10500);
+
+// // database.ref('location/city')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((e) => {
+// //     console.log('Error fetching data', e);
+// //   });
+
+// // database.ref().set({
+// //     name: 'Andrew Mead',
+// //     age: 26,
+// //     stressLevel: 6,
+// //     job: {
+// //         title: 'Software developer',
+// //         company: 'Google'
+// //     },
+// //     location: {
+// //         city: 'Philadelphia',
+// //         country: 'United States'
+// //     }
+// // }).then(() => {
+// //     console.log('Data is saved');
+// // }).catch((e) => {
+// //     console.log('This failed.', e);
+// // });
+
+// // database.ref().update({
+// //     stressLevel: 9,
+// //     'job/company': 'Amazon',
+// //     'location/city': 'Seattle'
+// // });
+
+// // database.ref('isSingle').set(null);
+
+// // database.ref()
+// //     .remove()
+// //     .then(() => {
+// //         console.log('Data was removed');
+// //     }).catch((e) => {
+// //         console.log('Did not remove data', e);
+// //     });
--- expensify-app/src/playground/promises.js
+++ expensify-app/src/playground/promises.js
@@ -1,18 +1,26 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'Andrew',
-        //     age: 26
-        // });    
-        reject('Something went wrong!')    
-    }, 1500);
+        resolve({
+            name: 'Andrew',
+            age: 26
+        });    
+        // reject('Something went wrong!')    
+    }, 5000);
 });
 
 console.log('before');
 
 promise.then((data) => {
     console.log('1', data);
-}, (error) => {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('This is my other promise');       
+        }, 5000);
+    })
+}).then((str) => {
+    console.log('does this run?', str);
+}).catch((error) => {
     console.log('error', error);
 });
 
