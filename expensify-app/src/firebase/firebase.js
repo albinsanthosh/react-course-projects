be51c351dfc2a83798b7f5bca742bed373b4fda2
VF-ROOT\santa003 <albin.santhosh@vcontractor.co.za>
2024-07-17 10:49:36 +0530

    test database real

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD4L4uOR1F4zE3yhX7gWD4Wi0cSzxIXzBE",
-    authDomain: "expensify-886ac.firebaseapp.com",
-    databaseURL: "https://expensify-886ac-default-rtdb.firebaseio.com",
-    projectId: "expensify-886ac",
-    storageBucket: "expensify-886ac.appspot.com",
-    messagingSenderId: "659032055673",
-    appId: "1:659032055673:web:a070cc0779f8d5ceaf99a8"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
   
 // Initialize Firebase
